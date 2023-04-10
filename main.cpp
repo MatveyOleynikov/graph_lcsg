169209b2c28f6ea6371a7fbef4fa235b8f0b13d0
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-10 14:07:55 +0300

    made operator >> for graph

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ class graph{
     vector<vector<int>> adjacency_list;
     int n;
     int m;
+
+    friend istream& operator>>(istream& o, graph& cur)
+    {
+        for (long long i = 0; i < cur.m; ++i){
+            long long u, v;
+            o >> u; u--;
+            o >> v; v--;
+            cur.adjacency_list[u].push_back(v);
+            cur.adjacency_list[v].push_back(u);
+        }
+        return o;
+    }
 };
 
 int main(){
