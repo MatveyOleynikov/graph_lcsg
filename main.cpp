a6d87b332413d31d76473a0800f8b76addef6993
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-10 14:12:48 +0300

    made every adjacent unique

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class graph{
-    vector<vector<int>> adjacency_list;
+    vector<unordered_set<int>> adjacency_list;
     int n;
     int m;
 
@@ -14,7 +15,7 @@ class graph{
             long long u, v;
             o >> u; u--;
             o >> v; v--;
-            cur.adjacency_list[u].push_back(v);
+            cur.adjacency_list[u].insert(v);
         }
         return o;
     }
