12aaa59048e6c6337768cfc771fd16db89c2f438
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-10 14:06:27 +0300

    made class graph

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class graph{
+    vector<vector<int>> adjacency_list;
+    int n;
+    int m;
+};
+
 int main(){
-  return 0;
+
+    return 0;
 }
